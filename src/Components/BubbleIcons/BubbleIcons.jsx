d179be7481195e27d2ad1fe5cf5407c9bad4dcad
IsidoreSKO <sparagmos@hotmail.fr>
2023-12-06 18:51:47 +0100

    test1

--- src/Components/BubbleIcons/BubbleIcons.jsx
+++ src/Components/BubbleIcons/BubbleIcons.jsx
@@ -1,14 +1,16 @@
 import "./bubbleIcons.css";
 import dataSkills from "../../Assets/Data/dataSkills.json";
 
-import { useParallax } from "react-scroll-parallax";
+// import { useParallax } from "react-scroll-parallax";
 
 function BubbleIcons() {
-  const parallax = useParallax({ scale: [0, 0.9] });
+  // const parallax = useParallax({ scale: [0, 0.9] });
   const bubblesData = dataSkills;
 
   return (
-    <ul ref={parallax.ref}>
+    <ul
+    // ref={parallax.ref}
+    >
       {bubblesData.map((bubble) => (
         <li className="icons" key={bubble.id}>
           <img src={bubble.image} alt={bubble.description} />
